6d9fb446ab25803aec0aa8112bebd87ef217101f
Marcel Baumgartner <marcel.kbkm@gmail.com>
2021-10-20 17:52:59 +0200

    Added GetContainers to DockerController

--- TabsyDaemon/Controller/DockerController.cs
+++ TabsyDaemon/Controller/DockerController.cs
@@ -1,13 +1,49 @@
-﻿using System;
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
+using TabsyDaemon.Docker;
+using TabsyDaemon.Services;
 
 namespace TabsyDaemon.Controller
 {
     public class DockerController
     {
+        private static DockerClient DockerClient { get { return DockerService.DockerClient; } }
 
+        public async Task<List<DockerContainer>> GetContainers()
+        {
+            List<DockerContainer> result = new List<DockerContainer>();
+
+            IList<ContainerListResponse> containers = await DockerClient.Containers.ListContainersAsync(
+            new ContainersListParameters());
+
+            foreach(ContainerListResponse clr in containers)
+            {
+                DockerContainer c = new DockerContainer()
+                {
+                    Command = clr.Command,
+                    Created = clr.Created,
+                    ID = clr.ID,
+                    Image = clr.Image,
+                    ImageID = clr.ImageID,
+                    Labels = clr.Labels,
+                    Mounts = clr.Mounts,
+                    Names = clr.Names,
+                    NetworkSettings = clr.NetworkSettings,
+                    Ports = clr.Ports,
+                    SizeRootFs = clr.SizeRootFs,
+                    SizeRw = clr.SizeRw,
+                    State = clr.State,
+                    Status = clr.Status
+                };
+
+                result.Add(c);
+            }
+
+            return result;
+        }
     }
 }
---
+++ TabsyDaemon/Docker/DockerContainer.cs
@@ -0,0 +1,25 @@
+﻿using Docker.DotNet.Models;
+
+using System;
+using System.Collections.Generic;
+
+namespace TabsyDaemon.Docker
+{
+    public class DockerContainer
+    {
+        public string ID { get; set; }
+        public IList<string> Names { get; set; }
+        public string Image { get; set; }
+        public string ImageID { get; set; }
+        public string Command { get; set; }
+        public DateTime Created { get; set; }
+        public IList<Port> Ports { get; set; }
+        public long SizeRw { get; set; }
+        public long SizeRootFs { get; set; }
+        public IDictionary<string, string> Labels { get; set; }
+        public string State { get; set; }
+        public string Status { get; set; }
+        public SummaryNetworkSettings NetworkSettings { get; set; }
+        public IList<MountPoint> Mounts { get; set; }
+    }
+}
