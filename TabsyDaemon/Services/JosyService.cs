9e521d58ea019bf1f8a5bbe46ded2eb88faa0d95
Marcel Baumgartner <marcel.kbkm@gmail.com>
2021-10-21 17:15:48 +0200

    Added JosyService and JosyCallbacks Library

---
+++ TabsyDaemon/Services/JosyService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+using TabsyDaemon.Interfaces;
+
+namespace TabsyDaemon.Services
+{
+    public class JosyService : ITabsyService
+    {
+        public bool IsRunning => throw new System.NotImplementedException();
+
+        public Task Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
