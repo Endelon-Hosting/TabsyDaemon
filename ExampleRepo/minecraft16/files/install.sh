a10d11c1a335f154f71c88057e43e0ccad04bb52
Marcel Baumgartner <marcel.kbkm@gmail.com>
2021-10-25 16:23:21 +0200

    Added Example Repo

---
+++ ExampleRepo/minecraft16/files/install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Installing Minecraft Server"
+wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/353/downloads/paper-1.17.1-353.jar -O /root/server/server.jar
